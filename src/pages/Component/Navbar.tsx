97b0746823d223b6ea3774be07ce951f35639f4f
Johan RAFANOMEZANTSOA (DSI) <ST121@ravinala-airports.aero>
2024-09-03 15:27:03 +0300

    working on home page

---
+++ src/pages/Component/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <div className="fixed flex items-center justify-between backdrop-blur-3xl border rounded-xl border-gray-300 py-3 px-4">
+            <h1 className="">Navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar
